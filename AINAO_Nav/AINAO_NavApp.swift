9ae319f1e993e1007e371c77a14f1afc729f3627
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-08-14 01:04:46 +0200

    Initial Commit

---
+++ AINAO_Nav/AINAO_NavApp.swift
@@ -0,0 +1,17 @@
+//
+//  AINAO_NavApp.swift
+//  AINAO_Nav
+//
+//  Created by  Ixart on 14/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct AINAO_NavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
